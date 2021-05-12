58b2c0b2bae3c5e8a1238ca576c1eb0b92fd3728
ithsn8@gmail.com <ithsn8@gmail.com>
2021-05-12 09:36:12 +0300

    Add project files.

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -42,7 +42,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
-
+         
 
         [HttpPost("delete")]
         public IActionResult Delete(User user)
