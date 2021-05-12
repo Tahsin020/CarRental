f1d6a386f1f3c9cfe8d3c512532c28441a077c28
ithsn8@gmail.com <ithsn8@gmail.com>
2021-05-12 09:34:19 +0300

    .

--- Core/Utilities/Security/Hashing/HashingHelper.cs
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Core.Utilities.Security.Hashing
+namespace Core.Utilities.Security.Hashing    
 {
     public class HashingHelper
     {
